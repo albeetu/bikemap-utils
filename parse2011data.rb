b07fd2676f3f10ae2cf3d25fa01d39181666140b
Albert Tu <albeetu@gmail.com>
2013-01-15 20:46:16 -0800

    Added in critical data grabber

--- parse2011data.rb
+++ parse2011data.rb
@@ -14,28 +14,44 @@
 require 'rubygems'
 require 'pp'
 
+
+
 def readfile(file)
   incidents = Array.new()
+  refined = Array.new()
+  grabber = Hash.new()
   block = String.new
   counter = 0
   incident_file = File.new(file)
   while (line = incident_file.gets)
     if (line =~ /Primary Rd/)
+      grabber['Primary Rd'] = line.chomp
+      refined[counter] = grabber
       incidents[counter] = block
       counter = counter + 1
       block = ""
     end
+    if (line =~ /Secondary Rd/)
+      grabber['Secondary Rd'] = line.chomp
+    end
+    if (line =~/Collision Date/)
+      #grab next line
+      grabber['Collision Date'] = line.chomp + ', ' + incident_file.gets.chomp
+    end
+    if (line =~/Collision Type/) 
+      grabber['Collision Type'] = incident_file.gets.chomp
+    end
     block += line
   end
   puts "#{counter} incidents"
-  return incidents
+  return [refined,incidents]
 end
- 
+
 def main()
   incidents = Array.new()
+  refined = Array.new()
   incidents = readfile('labikemap2011.txt')
-  pp incidents
-  puts incidents.count
+  pp incidents[0]
 end
 
 main()
