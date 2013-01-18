7f5851fb657152da9f757e48372188874a759011
Albert <albeetu@gmail.com>
2013-01-17 17:31:01 -0800

    Fix geocoder

--- bikemap-readcsv.php
+++ bikemap-readcsv.php
@@ -34,12 +34,25 @@ function readHeader($header){
   
 }
 
-function geocode($street, $crossstreet, $offset){
-  $intersection = $street." / ".$crosstreet.", Los Angeles, CA";
-  $url = "http://dev.dma.ucla.edu/~mpeteu/bikemap/geocode-api.php?loc=".$intersection;
-  $geo_return = file_get_contents($url);
-  $coords = explode(",", $geo_return);
-  return $coords;
+function geocode($street, $crosstreet, $offset){
+  $ch = curl_init();
+
+  // set URL and other appropriate options
+  $address = urlencode($street. " and ".$crosstreet." Los Angeles,CA");
+  curl_setopt($ch, CURLOPT_URL, "http://maps.googleapis.com/maps/api/geocode/json?address=" . $address. "&sensor=false");
+  curl_setopt($ch, CURLOPT_HEADER, 0);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  // grab URL and pass it to the browser
+  $output  = curl_exec($ch);
+  //echo $output;
+
+  // close cURL resource, and free up system resources
+  curl_close($ch);
+
+  $feed = json_decode( $output, true);
+
+  $retString = $feed['results'][0]['geometry']['location']['lat'] . ',' . $feed['results'][0]['geometry']['location']['lng'];
+  return explode(",",$retString);
 }
 
 function readRecord($list,$contents){
@@ -62,12 +75,21 @@ function readRecord($list,$contents){
   return $records;
 }
 
-function getNecessary($list,$contents){
+function getNecessary($list,$contents,$limit){
 
-  $selection = Array(0,4,5,18,19,20,21,23,36,37,38,42,43,44,45,46,47,49,54,66,68);
+  
+  //$selection = Array(0,4,5,18,19,20,21,23,36,37,38,42,43,44,45,46,47,49,54,66,68);
+  $selection = Array(0,18,19);
   $records = Array();
   $record_count = 0;
-  for ($i=1; $i<count($contents); $i++)
+  if ($limit){ 
+    $records_needed = $limit;
+  }
+  else
+  {
+    $records_needed = count($contents);
+  }
+  for ($i=1; $i<$records_needed; $i++)
   {
     $record = explode(',',$contents[$i]);
     foreach($selection as $index)
@@ -75,8 +97,9 @@ function getNecessary($list,$contents){
         $data_record[$list[$index]] = trim($record[$index],'"\n');
     }
     $coords = geocode($data_record["primary_rd"],$data_record["secondary_rd"],0);
-    $data_record["long"] = $coords[0];
-    $data_record["lat"] = $coords[1];
+    usleep(50000);
+    $data_record["lat"] = $coords[0];
+    $data_record["long"] = $coords[1];
     $records[$record_count] = $data_record;
     $record_count++;
   }
@@ -89,7 +112,7 @@ function main(){
  $field_list = readHeader($s_contents[0]);
  //print_r($field_list);
  $records = readRecord($field_list,$s_contents);
- $necessary = getNecessary($field_list,$s_contents);
+ $necessary = getNecessary($field_list,$s_contents,0);
  print json_encode($necessary);
  //print json_encode($records);
 }
